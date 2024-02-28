5b6e623eb02f3412900c4b4a9300ddd5661a05ed
bezkagoit <142536080+bezkagoit@users.noreply.github.com>
2024-02-28 11:40:23 +0000

    Created a structure and all tasks

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import "./App.css";
+
+import Profile from "./components/Profile/Profile.jsx";
+import FriendList from "./components/FriendList/FriendList.jsx";
+import TransactionHistory from "./components/TransactionHistory/TransactionHistory.jsx";
+
+import userData from "./components/Profile/userData.json";
+import friends from "./components/FriendList/friends.json";
+import transactions from "./components/TransactionHistory/transactions.json";
+
+function App() {
+  return (
+    <>
+      <Profile
+        username={userData.username}
+        tag={userData.tag}
+        location={userData.location}
+        avatar={userData.avatar}
+        stats={userData.stats}
+      />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/FriendList/FriendList.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import FriendListItem from "./FriendListItem";
+import css from "./FriendList.module.css";
+
+const FriendList = ({ friends }) => {
+  return (
+    <ul className={css.list}>
+      {friends.map(({ avatar, name, isOnline, id }) => (
+        <FriendListItem
+          key={id}
+          avatar={avatar}
+          name={name}
+          isOnline={isOnline}
+        />
+      ))}
+    </ul>
+  );
+};
+
+export default FriendList;
